561a0dd2610b87c375b6991e04026db28ed82e64
Marian Schubert <maio@MacBook-Pro.local>
2016-10-24 06:52:07 +0200

    Introduce config.js
    Which should keep all things related to system configuration.

---
+++ web/src/config.js
@@ -0,0 +1,11 @@
+var db = {
+  user: process.env.PGUSER || 'postgres',
+  database: process.env.PGDATABASE || 'postgres',
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST || 'localhost',
+  port: process.env.PGPORT || 5432,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long to keep idle client
+}
+
+exports.db = db
