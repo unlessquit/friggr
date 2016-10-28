fd566b21c39f75c207b305bd79ba7e944398e784
Marian Schubert <marian.schubert@gmail.com>
2016-10-28 19:54:01 +0200

    Inject storage dependency

--- web/src/app.js
+++ web/src/app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
-var mainRoutes = require('./main')
+var storage = require('./storage')
+var mainRoutes = require('./main').build(storage)
 
 app.use('/', mainRoutes)
 
--- web/src/main.js
+++ web/src/main.js
@@ -1,52 +1,55 @@
 var express = require('express')
-var router = express.Router()
 var multer = require('multer')
 var db = require('./db')
-var storage = require('./storage')
 
 var upload = multer({ dest: '/tmp/' })
 
-router.get('/', function (req, res) {
-  db.query('SELECT $1::text as name', ['World'])
-    .then((result) => {
-      res.send('Hello ' + result.rows[0].name + '!')
-    })
-    .catch((error) => {
-      res.send(error.message)
-    })
-})
+exports.build = function (storage) {
+  var router = express.Router()
+  router.get('/', function (req, res) {
+    db.query('SELECT $1::text as name', ['World'])
+      .then((result) => {
+        res.send('Hello ' + result.rows[0].name + '!')
+      })
+      .catch((error) => {
+        res.send(error.message)
+      })
+  })
 
-router.post('/inbox', upload.single('photoFile'), function (req, res) {
-  var userId = req.body.userId
+  router.post('/inbox', upload.single('photoFile'), function (req, res) {
+    var userId = req.body.userId
 
-  storage.addPhotoFile(userId, req.file.path)
-    .then(photoInfo => res.redirect(viewPhotoPath(photoInfo)))
-    .catch(error => {
-      if (error instanceof storage.AccessDeniedError) {
-        res.status(403).send('Forbidden')
-      } else {
-        throw error
-      }
-    })
-    .catch(internalErrorHandler(res))
-})
+    storage.addPhotoFile(userId, req.file.path)
+      .then(photoInfo => res.redirect(viewPhotoPath(photoInfo)))
+      .catch(error => {
+        if (error instanceof storage.AccessDeniedError) {
+          res.status(403).send('Forbidden')
+        } else {
+          throw error
+        }
+      })
+      .catch(internalErrorHandler(res))
+  })
 
-router.get('/view/:userId/latest.jpg', function (req, res) {
-  var userId = req.params.userId
+  router.get('/view/:userId/latest.jpg', function (req, res) {
+    var userId = req.params.userId
 
-  storage.getLatestPhoto(userId)
-    .then(photoInfo => res.redirect(viewPhotoPath(photoInfo)))
-    .catch(error => {
-      if (error instanceof storage.NotFoundError) {
-        res.status(404).send('Not Found')
-      } else {
-        throw error
-      }
-    })
-    .catch(internalErrorHandler(res))
-})
+    storage.getLatestPhoto(userId)
+      .then(photoInfo => res.redirect(viewPhotoPath(photoInfo)))
+      .catch(error => {
+        if (error instanceof storage.NotFoundError) {
+          res.status(404).send('Not Found')
+        } else {
+          throw error
+        }
+      })
+      .catch(internalErrorHandler(res))
+  })
 
-router.use('/view', express.static(storage.filesRoot, {maxAge: '1year'}))
+  router.use('/view', express.static(storage.filesRoot, {maxAge: '1year'}))
+
+  return router
+}
 
 function viewPhotoPath (photoInfo) {
   return '/view/' + photoInfo.userId + '/' + photoInfo.id + '.jpg'
@@ -58,5 +61,3 @@ function internalErrorHandler (res) {
     res.status(500).send('Internal Error')
   }
 }
-
-module.exports = router
