19e89022a9d32b82af06d5ab0ad8c4d18cd02e36
Marian Schubert <marian.schubert@gmail.com>
2017-01-23 08:40:44 +0100

    Display resized photos to users (2000x2000)

--- web/client/src/components/ViewPhoto.js
+++ web/client/src/components/ViewPhoto.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 export default ({photo}) => {
-  var {userId, id} = photo
-  var url = '/view/' + encodeURIComponent(userId) +
-      '/' + encodeURIComponent(id) + '.jpg'
+  var url = viewPhotoPath(photo)
 
   return (
     <div className='view-page'>
       <img className='photo' role='presentation' src={url} />
     </div>
   )
+}
+
+function viewPhotoPath (photoInfo) {
+  return '/view/' + photoInfo.userId + '/' + photoInfo.id + '/2000/2000'
 }
