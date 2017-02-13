889ce2a0e9c24dea70ed8bfe5935ecd48d8e61df
Marian Schubert <marian.schubert@gmail.com>
2017-02-13 08:47:38 +0100

    Convert ViewPhoto to Component
    So that it can hold local state.

--- web/client/src/components/ViewPhoto.js
+++ web/client/src/components/ViewPhoto.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 function Caption (caption) {
   if (!caption) return ''
@@ -6,18 +6,25 @@ function Caption (caption) {
   return <div className='caption'><span>{caption}</span></div>
 }
 
-export default ({photo}) => {
-  var url = viewPhotoPath(photo)
+export default class ViewPhoto extends Component {
+  constructor ({dispatch}) {
+    super()
+  }
 
-  // img key attribute forces immediate display of new photo even when
-  // it's not yet loaded (giving user feedback that his action has
-  // been registered).
-  return (
-    <div className='view-page'>
-      <img key={photo.id} className='photo' role='presentation' src={url} />
-      {Caption(photo.caption)}
-    </div>
-  )
+  render () {
+    var photo = this.props.photo
+    var url = viewPhotoPath(photo)
+
+    // img key attribute forces immediate display of new photo even when
+    // it's not yet loaded (giving user feedback that his action has
+    // been registered).
+    return (
+      <div className='view-page'>
+        <img key={photo.id} className='photo' role='presentation' src={url} />
+        {Caption(photo.caption)}
+      </div>
+    )
+  }
 }
 
 function viewPhotoPath (photoInfo) {
